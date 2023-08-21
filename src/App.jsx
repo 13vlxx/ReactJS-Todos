c4c4eaf3d66e6069793c1987674607a399bfb2a0
ALX <alexmonac13@gmail.com>
2023-08-21 02:20:52 +0200

    [Update] Add new todos func

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,63 @@
-import { useState } from "react"
+import { useState } from "react";
 import { nanoid } from "nanoid";
 import ListItem from "./components/ListItem";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
 
-  const [todoList, settodoList] = useState([
-    { id: nanoid(8), content: "item 1" },
-    { id: nanoid(8), content: "item 2" },
-    { id: nanoid(8), content: "item 3" },
-  ]);
+  const [todo, setTodo] = useState("");
+  const [showValidation, setShowValidation] = useState(false);
 
   function deleteTodo(id) {
-    settodoList(todoList.filter(todo => todo.id !== id))
+    setTodoList(todoList.filter((todo) => todo.id !== id));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (todo === "") {
+      setShowValidation(true);
+      return;
+    }
+    setTodoList([...todoList, { id: nanoid(8), content: todo }]);
+    setTodo("");
+    setShowValidation(false);
   }
 
   return (
-    <div className="h-screen bg-slate-900">
+    <div className="h-screen">
       <div className="max-w-4xl mx-auto pt-20 px-6">
         <h1 className="text-3xl text-slate-100 mb-4">ToDo List</h1>
 
-        <form className="mb-10">
-          <label htmlFor="todo-item" className="text-slate-50">Ajouter une chose à faire</label>
-          <input type="text" className="mt-1 block w-full rounded" id="todo-item" />
+        <form onSubmit={handleSubmit} className="mb-10">
+          <label htmlFor="todo-item" className="text-slate-50">
+            Ajouter une chose à faire
+          </label>
+          <input
+            placeholder="Apprendre ReactJS"
+            type="text"
+            className="mt-1 block w-full rounded"
+            id="todo-item"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          {showValidation && (
+            <p className="text-red-400">Ajoutez d&apos;abord du contenu à votre tâche</p>
+          )}
           <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
         </form>
         <ul>
           {todoList.length === 0 && (
             <li className="text-slate-50 text-md">Pas d&apos;items à afficher...</li>
           )}
-          {todoList.map(item => (
-            <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo} />
-          ))}
+          {todoList.length > 0 &&
+            todoList.map((item) => (
+              <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo} />
+            ))}
         </ul>
-      </div >
-    </div >
-  )
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,8 +2,13 @@ function ListItem({ itemData, deleteTodo }) {
     return (
         <li className="p-2 bg-zinc-200 mb-2 rounded flex">
             <span>{itemData.content}</span>
-            <button onClick={() => deleteTodo(itemData.id)} className="ml-auto bg-red-600 w-6 h-6 rounded text-zinc-200">X</button>
+            <button
+                onClick={() => deleteTodo(itemData.id)}
+                className="ml-auto bg-red-600 w-6 h-6 rounded text-zinc-200"
+            >
+                X
+            </button>
         </li>
-    )
+    );
 }
-export default ListItem
+export default ListItem;
