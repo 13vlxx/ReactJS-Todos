33bec39c1a4a820d78736f64602f846f88adb177
ALX <alexmonac13@gmail.com>
2023-08-21 02:01:46 +0200

    [ Update ] add delete func

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react"
+import { nanoid } from "nanoid";
+import ListItem from "./components/ListItem";
 
 function App() {
 
+  const [todoList, settodoList] = useState([
+    { id: nanoid(8), content: "item 1" },
+    { id: nanoid(8), content: "item 2" },
+    { id: nanoid(8), content: "item 3" },
+  ]);
+
+  function deleteTodo(id) {
+    settodoList(todoList.filter(todo => todo.id !== id))
+  }
+
   return (
-    <>
-      <h1 className="bg-green-500 py-16">Git init.</h1>
-    </>
+    <div className="h-screen bg-slate-900">
+      <div className="max-w-4xl mx-auto pt-20 px-6">
+        <h1 className="text-3xl text-slate-100 mb-4">ToDo List</h1>
+
+        <form className="mb-10">
+          <label htmlFor="todo-item" className="text-slate-50">Ajouter une chose à faire</label>
+          <input type="text" className="mt-1 block w-full rounded" id="todo-item" />
+          <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
+        </form>
+        <ul>
+          {todoList.length === 0 && (
+            <li className="text-slate-50 text-md">Pas d&apos;items à afficher...</li>
+          )}
+          {todoList.map(item => (
+            <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo} />
+          ))}
+        </ul>
+      </div >
+    </div >
   )
 }
 
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,9 @@
+function ListItem({ itemData, deleteTodo }) {
+    return (
+        <li className="p-2 bg-zinc-200 mb-2 rounded flex">
+            <span>{itemData.content}</span>
+            <button onClick={() => deleteTodo(itemData.id)} className="ml-auto bg-red-600 w-6 h-6 rounded text-zinc-200">X</button>
+        </li>
+    )
+}
+export default ListItem
