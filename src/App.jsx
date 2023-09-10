7e5ab0732212aeeb5c3acbe923c742270b7700e6
Alex <alexmonac13@gmail.com>
2023-09-10 02:06:29 +0200

    input easteregg

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import TodoList from "./components/TodoList";
 function App() {
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
-  const [emptyInputError, setEmptyInputError] = useState(false);
-  const [todoAlreadyExist, setTodoAlreadyExist] = useState(false);
+  const [errors, setErrors] = useState({
+    emptyInput: false,
+    todoAlreadyExist: false,
+  });
 
   useEffect(() => {
     const storedTodoList = localStorage.getItem("todoList");
@@ -21,22 +23,21 @@ function App() {
     e.preventDefault();
 
     if (!todo) {
-      setEmptyInputError(true);
+      setErrors({ ...errors, emptyInput: true });
       return;
     }
 
-    const todoAlreadyExist = todoList.some((item) => item.text === todo);
+    const tae = todoList.some((item) => item.text === todo);
 
-    if (todoAlreadyExist) {
-      setTodoAlreadyExist(true);
+    if (tae) {
+      setErrors({ ...errors, todoAlreadyExist: true });
       return;
     }
 
     const newTodoList = [...todoList, { id: nanoid(8), text: todo }];
     setTodoList(newTodoList);
     setTodo("");
-    setEmptyInputError(false);
-    setTodoAlreadyExist(false);
+    setErrors({ ...errors, todoAlreadyExist: false, emptyInput: false });
 
     localStorage.setItem("todoList", JSON.stringify(newTodoList));
   }
@@ -44,23 +45,33 @@ function App() {
   function deleteTodo(id) {
     const newTodoList = todoList.filter((todo) => todo.id !== id);
     setTodoList(newTodoList);
+    setErrors({ ...errors, todoAlreadyExist: false, emptyInput: false });
 
     localStorage.setItem("todoList", JSON.stringify(newTodoList));
   }
 
   let errorMessage;
-  if (todoList.length !== 0) {
-    if (emptyInputError) {
-      errorMessage = <p className="mt-2 text-red-500">Veuillez bien remplir le champ...</p>;
-    } else if (todoAlreadyExist) {
+  if (todoList.length === 0) {
+    errorMessage = <p className="mt-2">Aucune tache à afficher...</p>;
+  } else {
+    if (errors.todoAlreadyExist) {
       errorMessage = <p className="mt-2 text-red-500">Tache déjà existante...</p>;
     }
-  } else {
-    errorMessage = <p className="mt-2">Aucune tache à afficher...</p>;
+    if (errors.emptyInput) {
+      errorMessage = <p className="mt-2 text-red-500">Veuillez bien remplir le champ...</p>;
+    }
+  }
+
+  if (errors.emptyInput) {
+    errorMessage = <p className="mt-2 text-red-500">Veuillez bien remplir le champ...</p>;
   }
 
   return (
-    <div className="min-h-screen bg-slate-900 text-slate-50 p-8">
+    <div
+      className={`min-h-screen transition-all duration-500 bg-slate-900 text-slate-50 p-8 ${
+        todo === "vertical" && "cursor-vertical-text"
+      } ${todo.includes("13vlxx") && "bg-slate-200 text-slate-950"}`}
+    >
       <h1 className="text-3xl font-semibold select-none">React Todos</h1>
       <form onSubmit={handleSubmit} className="flex gap-2 mt-4">
         <input
@@ -70,14 +81,23 @@ function App() {
           className="text-slate-950 rounded w-full py-2 pl-2 outline-blue-600"
           type="text"
         />
-        <button className="bg-blue-600 px-2 rounded hover:bg-blue-700 transition-colors duration-100">
+        <button
+          disabled={!todo}
+          className={`bg-blue-600 px-2 rounded hover:bg-blue-700 transition-colors duration-200 ${
+            !todo && "bg-gray-500 hover:bg-gray-600 cursor-not-allowed"
+          } ${
+            todo.includes("13vlxx")
+              ? "bg-green-500 hover:bg-green-600 cursor-cell text-slate-50"
+              : ""
+          }`}
+        >
           Valider
         </button>
       </form>
       {errorMessage}
-      <ul className="mt-4">
+      <ul className={`mt-4 ${todo.includes("13vlxx") ? "text-slate-50" : ""}`}>
         {todoList.map((todo) => (
-          <TodoList key={todo.id} content={todo} deleteTodo={deleteTodo} />
+          <TodoList key={todo.id} content={todo} deleteTodo={deleteTodo} inputValue={todo} />
         ))}
       </ul>
     </div>
